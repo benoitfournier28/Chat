196652ac6136f4bd611afdfcb6d49b5038d8455a
Benoît Fournier <benoitfournier28pro@gmail.com>
2020-11-02 17:01:02 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`)
+});
+
+io.on('connection', (socket) => {
+    console.log('Un utilisateur s\'est connecté.');
+
+    socket.on('disconnect', () => {
+        console.log('un utilisateur s\'est déconnecté');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message reçu : ' + msg);
+        io.emit('chat message', msg );
+    });
+});
+
+
+
+server.listen(3000, () => {
+    console.log('server is listening on the port 3000.')
+});
+
+// io.on('connection', (socket) => {
+//     console.log('Un utilisateur s\'est connecté.');
+
+//     socket.on('chat message', (msg) => {
+//         console.log('Message:' + msg);
+//     });
+// });
