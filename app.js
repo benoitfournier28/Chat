531d9045e4536671e7ba2e489f9ca21a5eb150b4
Benoît Fournier <benoitfournier28pro@gmail.com>
2020-11-03 13:25:41 +0100

    sass init

--- app.js
+++ app.js
@@ -5,7 +5,20 @@ const io = require('socket.io')(server);
 
 const messages = require("./back/message");
 
+const sassMiddleware = require("node-sass-middleware");
+const pathSass = require("path");
+
 app.use(express.static('public'));
+
+app.use(sassMiddleware({
+    /* Options */
+    src: pathSass.join(__dirname, 'public'),
+    dest: pathSass.join(__dirname, 'public'),
+    debug: true,
+    outputStyle: 'compressed',
+    indentedSyntax: false,
+    // prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
+}));
 
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`)
